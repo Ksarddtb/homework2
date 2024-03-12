70262d32ea6164c821a08fe6df0fb38b97d099c1
ksard <flastdtb@yandex.ru>
2024-03-12 22:51:10 +0500

    work3

---
+++ work3/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = 0;
+Console.WriteLine("Введите число из отрезка [10, 99]");
+number = Convert.ToInt32(Console.ReadLine());
+if (number < 10 && number > 99)
+{
+    Console.WriteLine("Не соотвествует деапозону  [10, 99]");
+}
+else
+{
+    int num1 = number / 10;
+    int num2 = number % 10;
+    if (num1 > num2)
+    {
+        Console.WriteLine(num1 + " больше");
+    }
+    else
+    {
+        Console.WriteLine(num2 + " больше");
+    }
+}

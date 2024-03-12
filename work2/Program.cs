94e24d615216509b5ce6f33c6ef6b2256ac8713e
ksard <flastdtb@yandex.ru>
2024-03-12 22:31:25 +0500

    work2

---
+++ work2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Программа определание четверти по координатам!");
+
+int x = 0;
+int y = 0;
+
+Console.WriteLine("Введите X координаты:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y координаты:");
+y = Convert.ToInt32(Console.ReadLine());
+
+if (x==0 && y==0){
+    Console.WriteLine("Не правильное значение");
+}
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1 Четверт");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 Четверт");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 Четверт");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 Четверт");
+}

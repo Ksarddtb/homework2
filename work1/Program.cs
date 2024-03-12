e7520eeb0d7bcc06197bc846679e2d1bd106c4ea
ksard <flastdtb@yandex.ru>
2024-03-12 22:13:51 +0500

    work1

---
+++ work1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Введите число!");
+int number=Convert.ToInt32(Console.ReadLine());
+if(number % 7==0 || number % 23==0){
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Нет");
+}
